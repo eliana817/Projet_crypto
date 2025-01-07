8fc47d79db0b6bcbf354822c298cbc27f8e658fa
unknown <egrasser@gamingcampus.local>
2025-01-07 16:47:21 +0100

    fixed merges

--- V.2/app.py
+++ V.2/app.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from application import create_app
-from python_files import database
-
-app = create_app()  
-
-if __name__ == '__main__':
-    database.create_user_table()  # Crée la table des utilisateurs à chaque démarrage de l'application
-    database.create_votes_table()  # Crée la table des votes si elle n'existe pas
-    database.create_admin_user()
-    app.run(debug=True, port=5002)
-=======
 from application import create_app
 from python_files import database
 
@@ -21,4 +9,3 @@ if __name__ == '__main__':
     database.create_votes_table()  # Crée la table des votes si elle n'existe pas
     database.create_admin_user()
     app.run(debug=True, port=5002, ssl_context='adhoc')
->>>>>>> c23016a6ba736ddf207eeb97331defe7f2d51deb
