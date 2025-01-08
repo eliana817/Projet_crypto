91055084316bdab28799c5423ba458715fcb4763
Nephyun <nephyun@proton.me>
2025-01-08 15:24:51 +0100

    Add files via upload

--- V.2/app.py
+++ V.2/app.py
@@ -1,13 +1,10 @@
 from application import create_app
 from python_files import database
-import os
 
-app = create_app()  # L'appel de create_app s'assure que la clé secrète est bien générée dans init.py
+app = create_app()  
 
 if __name__ == '__main__':
-    # Création des tables si elles n'existent pas
-    database.create_user_table()  # Crée la table des utilisateurs
+    database.create_user_table()  # Crée la table des utilisateurs à chaque démarrage de l'application
     database.create_votes_table()  # Crée la table des votes si elle n'existe pas
-    database.create_admin_user()  # Crée un utilisateur administrateur si nécessaire
-
-    app.run(debug=True, port=5002, ssl_context='adhoc')  # SSL ad-hoc pour le développement
+    database.create_admin_user()
+    app.run(debug=True, port=5002, ssl_context=('cert.pem', 'key.pem'))
